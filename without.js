361ee40df23182597928bd53dc5e5165af6bb8b6
saoussen <sawsan_sli3i@live.fr>
2020-11-24 18:52:15 +0000

    implemented without.js

--- without.js
+++ without.js
@@ -1,6 +1,3 @@
-import baseDifference from './.internal/baseDifference.js'
-import isArrayLikeObject from './isArrayLikeObject.js'
-
 /**
  * Creates an array excluding all given values using
  * [`SameValueZero`](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero)
@@ -19,8 +16,41 @@ import isArrayLikeObject from './isArrayLikeObject.js'
  * without([2, 1, 2, 3], 1, 2)
  * // => [3]
  */
-function without(array, ...values) {
-  return isArrayLikeObject(array) ? baseDifference(array, values) : []
+const assertArraysEqual = function(actual, expected) {
+  if (!assertEqual(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`)
+  }
 }
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return false;
+  } else {
+    return true;
+  }
+}
+const eqArrays = function(arr1, arr2) {
+  let compare = true;
+  for (let i = 0 ; i < arr1.length;  i++) {
+    if (arr1[i]!== arr2[i]) {
+      compare = false;
 
-export default without
+    }
+  }
+ return (arr1.length !== arr2.length) ? false : compare;
+
+}
+function without(arr1, arr2) {
+  let newArr = [];
+  for (const a of arr1) {
+    if (!arr2.includes(a)) {
+      newArr.push(a);
+    }
+  }
+  return newArr;
+
+}
+assertArraysEqual(eqArrays(without([1, 2, 3], [1]), [5, 3]), true);
+
+
