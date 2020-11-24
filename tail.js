48d4ed5de28e6f92488f0097e4351ed6c6b75fbf
saoussen <sawsan_sli3i@live.fr>
2020-11-24 16:28:02 +0000

    called the assertEqual within tail.js

--- tail.js
+++ tail.js
@@ -10,6 +10,13 @@
  * tail([1, 2, 3])
  * // => [2, 3]
  */
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed ${actual}!== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed ${actual}=== ${expected}`)
+  }
+}
 function tail(array) {
   const length = array == null ? 0 : array.length
   if (!length) {
@@ -18,5 +25,8 @@ function tail(array) {
   const [, ...result] = array
   return result
 }
+const result = tail(["Hello", "Lighthouse", "Labs"])
+assertEqual(result.length, 2) // ensure we get back two elements
+assertEqual(result[0], "Lighthouse") // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs") // ensure second element is "Labs"
 
-export default tail
