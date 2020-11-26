7a59fdb26ed01d691744c95fca213f7c562e9512
saoussen <sawsan_sli3i@live.fr>
2020-11-26 14:45:28 +0000

    added assertions to  takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,21 @@
+const assertArraysEqual = function(actual, expected) {
+  if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed ${actual} === ${expected}`)
+  }
+}
+
+const eqArrays = function(arr1, arr2) {
+  let compare = true;
+  for (let i = 0 ; i < arr1.length;  i++) {
+    if (arr1[i]!== arr2[i]) {
+      compare = false;
+
+    }
+  }
+ return (arr1.length !== arr2.length) ? false : compare;
+}
 const takeUntil = function(array, callback) {
   // ...
   let returnArr = [];
@@ -19,3 +37,5 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual( results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
