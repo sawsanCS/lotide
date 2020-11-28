f6f86ff2a3dde9e6a203fe1a07e496195082478b
saoussen <sawsan_sli3i@live.fr>
2020-11-28 11:06:09 +0000

    added mocha tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,13 @@
 const tail = require('../tail')
-const assertEqual = require ('../assertEqual')
-
-const result = tail(['Hello', 'Lighthouse', 'Labs'])
-assertEqual(result.length, 2) // ensure we get back two elements
-assertEqual(result[0], 'Lighthouse') // ensure first element is "Lighthouse"
-assertEqual(result[1], 'Labs') // ensure second element is "Labs"
+const assert = require('chai').assert;
 
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs']
-tail(words) // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3)
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns [6,5] for [5]", () => {
+    assert.strictEqual(head([6,5]), [5]);
+  });
+
+});
